e65d192577e41e3da7e87a67104ea4eaee33e3d9
Your Name <you@example.com>
2023-08-03 02:01:34 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  int days = 30;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Catlog App"),
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Hello  to $days days of flutter"),
+        ),
+      ),
+    );
+  }
+}
