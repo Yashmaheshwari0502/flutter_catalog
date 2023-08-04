7d2e39d96b2dd8b5bb70dba8759967113a43fe73
Your Name <you@example.com>
2023-08-04 19:25:43 +0530

    day 9 completed

---
+++ lib/model/item.dart
@@ -0,0 +1,16 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final String price;
+  final String color;
+  final String image;
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+}
