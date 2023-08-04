5a179a9f3e62c12657c9143cea3eb8fe96407950
Your Name <you@example.com>
2023-08-04 19:15:10 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catlog/pages/home_page.dart';
 import 'package:flutter_catlog/pages/login_page.dart';
 import 'package:flutter_catlog/utils/routes.dart';
+import 'package:flutter_catlog/widget/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -16,10 +17,8 @@ class MyApp extends StatelessWidget {
     return (MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutess.homepage,
       // home: HomePage(),
       routes: {
